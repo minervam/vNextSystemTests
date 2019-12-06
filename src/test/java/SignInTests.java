e197d32ffda12b7ad6935e6f8e5e0cb0d9cc9950
mmendoza <mmendoza@godaddy.com>
2019-12-06 08:52:18 -0800

    Updated the tests with the new sign in screens.

--- src/test/java/SignInTests.java
+++ src/test/java/SignInTests.java
@@ -9,52 +9,87 @@ import static org.junit.Assert.*;
 public class SignInTests extends BaseClass {
 
     public void verifySignInScreenUI() {
-        MobileElement signInButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/sign_in_button");
-        MobileElement usernameField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/username");
-        MobileElement passwordField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/password");
+        MobileElement forgotButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/forgot");
+        MobileElement enterYourGDUsernameText = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/description");
+        MobileElement usernameField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/text_input_edit_text");
+        MobileElement nextButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/sign_in_button");
 
-        signInButton.isDisplayed();
+        forgotButton.isDisplayed();
+        enterYourGDUsernameText.isDisplayed();
+        nextButton.isDisplayed();
         usernameField.isDisplayed();
-        passwordField.isDisplayed();
     }
 
     @Test
     public void validSignIn() {
+        //enter valid username
         verifySignInScreenUI();
-        MobileElement usernameField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/username");
-        MobileElement passwordField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/password");
-        MobileElement signInButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/sign_in_button");
+        MobileElement usernameField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/text_input_edit_text");
+        MobileElement nextButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/sign_in_button");
 
         usernameField.sendKeys("onelocal");
-        passwordField.sendKeys("2525Smart");
-        signInButton.click();
+        nextButton.click();
 
-        MobileElement accountButton = (MobileElement) driver.findElementByAccessibilityId("Navigate up");
-        assertTrue(accountButton.isDisplayed());
+        //verify password screen displays
+        MobileElement backToUsenameButton = (MobileElement) driver.findElementByAccessibilityId("Navigate up");
+        MobileElement enterYourPasswordText= (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/description");
+        MobileElement forgotButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/forgot");
+        MobileElement passwordField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/text_input_edit_text");
+        MobileElement finePrintText = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/fine_print");
+        MobileElement signInButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/sign_in_button");
+
+        assertTrue(forgotButton.isDisplayed());
+        assertTrue(backToUsenameButton.isDisplayed());
+        assertTrue(enterYourPasswordText.isDisplayed());
+        assertTrue(passwordField.isDisplayed());
+        assertTrue(signInButton.isDisplayed());
+        assertTrue(finePrintText.isDisplayed());
+
+        //enter valid password
+        passwordField.sendKeys("2525Smart");
+        MobileElement showHideButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/input_action_text");
+
+        /*verify show hide button
+        code for show hide btn goes here*/
+
+        signInButton.click();
+        verifyHomeScreenUI();
     }
 
     @Test
     public void invalidSignIn() {
         WebDriverWait wait = new WebDriverWait(driver, 3);
 
-        //Sign in without username and password
-        MobileElement signInButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/sign_in_button");
-        signInButton.click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(String.valueOf("com.godaddy.smartline.alpha:id/error_message"))));
+        //Check if next button is disabled
+        MobileElement usernameField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/text_input_edit_text");
+        MobileElement nextButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/sign_in_button");
+        assertFalse(nextButton.isEnabled());
 
-        //Sign in with invalid username
-        MobileElement usernameField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/username");
-        MobileElement passwordField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/password");
+        //type 2 characters in username
+        usernameField.sendKeys("in");
+        assertFalse(nextButton.isEnabled());
 
+        //type in an invalid username
         usernameField.sendKeys("invalidUsername");
+        nextButton.click();
+        MobileElement passwordField = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/text_input_edit_text");
+        MobileElement signInButton = (MobileElement) driver.findElementById("com.godaddy.smartline.alpha:id/sign_in_button");
+        assertFalse(signInButton.isEnabled());
+
+        //enter a valid password but username is invalid
         passwordField.sendKeys("2525Smart");
         signInButton.click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(String.valueOf("com.godaddy.smartline.alpha:id/error_message"))));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(String.valueOf("com.godaddy.smartline.alpha:id/error_text"))));
 
-        //Sign in with invalid password
+        //Go back and enter a valid username
+        MobileElement backToUsername = (MobileElement) driver.findElementByAccessibilityId("Navigate up");
+        backToUsername.click();
         usernameField.sendKeys("onelocal");
-        passwordField.sendKeys("invalidpwd");
+        nextButton.click();
+
+        //enter an invalid password
+        passwordField.sendKeys("invalidPassword");
         signInButton.click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(String.valueOf("com.godaddy.smartline.alpha:id/error_message"))));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(String.valueOf("com.godaddy.smartline.alpha:id/error_text"))));
     }
 }
